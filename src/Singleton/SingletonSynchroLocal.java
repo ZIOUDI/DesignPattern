f0c2830d590faa11b19a36935f09ec64d337e7e5
zioudi sofiane <zioudisofien@gmail.com>
2021-01-19 01:45:40 +0100

    Synchronisation locale

---
+++ src/Singleton/SingletonSynchroLocal.java
@@ -0,0 +1,37 @@
+package Singleton;
+
+ 
+ 
+public class SingletonSynchroLocal {
+
+	/** Constructeur privé */
+	private SingletonSynchroLocal() {
+	}
+
+	/** Instance unique non préinitialisée */
+	private static SingletonSynchroLocal INSTANCE = null;
+
+	/** Point d'accès pour l'instance unique du singleton */
+    public static SingletonSynchroLocal getInstance() 
+    {           
+
+        if (INSTANCE == null)
+        {   
+            synchronized(SingletonSynchroLocal.class) // ++++++++++++++++++
+            {
+                if (INSTANCE == null)
+                {   INSTANCE = new SingletonSynchroLocal();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
+
+/**
+ * 
+ * supprimer la synchronisation globale sur la méthode,   et ne l'appliquer que dans le cas où l'instance doit être créée 
+ *  
+ *  ==============>  cette solution, appelée "double-checked locking"
+ * 
+ *******/
