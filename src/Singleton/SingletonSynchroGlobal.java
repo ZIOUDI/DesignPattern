2e75d4812f4d29e5388abcba2e7a186a28993162
zioudi sofiane <zioudisofien@gmail.com>
2021-01-19 01:37:55 +0100

    Synchronisation globale

---
+++ src/Singleton/SingletonSynchroGlobal.java
@@ -0,0 +1,31 @@
+package Singleton;
+
+ 
+ 
+public class SingletonSynchroGlobal {
+
+	/** Constructeur privé */
+	private SingletonSynchroGlobal() {
+	}
+
+	/** Instance unique non préinitialisée */
+	private static SingletonSynchroGlobal INSTANCE = null;
+
+	/** Point d'accès pour l'instance unique du singleton */
+    public static synchronized SingletonSynchroGlobal getInstance() // +++ synchronized
+    {           
+        if (INSTANCE == null)
+        {   INSTANCE = new SingletonSynchroGlobal(); 
+        }
+        return INSTANCE;
+    }
+}
+
+/**
+ * 
+ * 
+ * Le problème est ainsi résolu, mais au prix d'une pénalité sur les performances. Si le singleton est accédé
+ *  souvent (système de log...), le ralentissement de l'application peut être important.
+ * 
+ * 
+ *******/
